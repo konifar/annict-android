2cbd47cd7080f5a43514a8d531a8ea14f09b96b1
konifar <konifar@Konishi-no-MacBook-Pro.local>
2016-06-08 23:46:46 +0900

    Create user model :building_construction:

---
+++ app/src/main/java/com/konifar/annict/model/User.java
@@ -0,0 +1,45 @@
+package com.konifar.annict.model;
+
+import com.github.gfx.android.orma.annotation.Column;
+import com.github.gfx.android.orma.annotation.PrimaryKey;
+import com.github.gfx.android.orma.annotation.Table;
+import com.google.gson.annotations.SerializedName;
+
+import org.parceler.Parcel;
+
+import java.util.Date;
+
+@Parcel
+@Table
+public class User {
+
+    @PrimaryKey(auto = false)
+    @Column(indexed = true)
+    @SerializedName("id")
+    public long id;
+
+    @Column(indexed = true)
+    @SerializedName("username")
+    public String username;
+
+    @Column
+    @SerializedName("description")
+    public String description;
+
+    @Column
+    @SerializedName("url")
+    public String url;
+
+    @Column
+    @SerializedName("records_count")
+    public int recordsCount;
+
+    @Column
+    @SerializedName("created_at")
+    public Date createdAt;
+
+    public User() {
+        //
+    }
+
+}
