1ac1110695720f4f78e60b7e599125b219c1581d
konifar <yahpeycoy0403@gmail.com>
2016-06-18 14:29:28 +0900

    Cosmetic change

--- app/src/main/java/com/konifar/annict/model/SimpleEpisode.java
+++ app/src/main/java/com/konifar/annict/model/SimpleEpisode.java
@@ -1,10 +1,11 @@
 package com.konifar.annict.model;
 
+import android.support.annotation.Nullable;
+
 import com.github.gfx.android.orma.annotation.Column;
 import com.github.gfx.android.orma.annotation.PrimaryKey;
 import com.github.gfx.android.orma.annotation.Table;
 import com.google.gson.annotations.SerializedName;
-import com.google.repacked.antlr.v4.runtime.misc.Nullable;
 
 import org.parceler.Parcel;
 
